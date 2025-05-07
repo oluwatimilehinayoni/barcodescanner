2ad79612b34edb009b1ae0655e4921a57397a33e
oluwatimilehinayoni <developertimi@gmail.com>
2025-05-07 15:37:49 +0100

    basic structure

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import mysql.connector
 
 app = Flask(__name__)
+CORS(app)  # This enables requests from external frontends
 
 # Connect to MySQL
 db = mysql.connector.connect(
